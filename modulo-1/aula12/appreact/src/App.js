67a142ea216c435a2dd853f93e727a73790c63cb
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-26 12:13:33 -0300

    (26/02) Componentes React.

---
+++ modulo-1/aula12/appreact/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer';
+import Form from './form/Form';
+import Header from './Header';
+
+const  App = () => {
+
+  return (
+    <React.Fragment>
+    <Header />
+    <Form />
+    <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
