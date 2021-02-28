97f393213d9f195d91eaa82616bb8f1cd8a88f24
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-28 15:06:57 -0300

    (28/02) UseState com callback.

---
+++ modulo-3/aula02/appreact/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const  App = () => {
+
+  const [contar, setContar] = React.useState(1)
+  const [items, setItems] = React.useState(["Item 1"])
+
+  function handleClick(){
+    setContar(contar + 1)
+    setItems([...items, `item ${contar + 1}`])
+  } 
+    
+  
+
+  return (
+    <div>
+      <ul>
+        {items.map(item => (
+          <li key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleClick}>{contar}</button>
+    </div>
+  );
+}
+
+export default App;
+ 
