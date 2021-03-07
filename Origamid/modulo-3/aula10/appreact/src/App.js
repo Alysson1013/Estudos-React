49d6ddf435e951ccb29b984cb3a95a289b0a8409
Alysson Eduardo <alysson6801@hotmail.com>
2021-03-07 14:17:20 -0300

    (07/03) CustomHook de Fetch.

---
+++ Origamid/modulo-3/aula10/appreact/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import useFetch from './useFetch';
+
+const  App = () => {
+  const {request, data, loading, error} = useFetch()
+
+  React.useEffect(()=> {
+    async function fetchData(){
+      const {response, json} = await request('https://ranekapi.origamid.dev/json/api/produto/')
+    }
+    fetchData()
+  }, [request])
+
+  if (error) return <p>{error}</p>
+  if (loading) return <div>Carregando...</div>
+  if(data) return(
+    <div>
+      {data.map(produto => (
+        <div key={produto.id}>
+          <h1>{produto.nome}</h1>
+        </div>
+      ))}
+    </div>
+  );
+  else return null
+}
+
+export default App;
+ 
