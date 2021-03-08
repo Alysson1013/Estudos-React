05391530d0a8cf0959f44612ea334f671b32ec6e
Alysson Eduardo <alysson6801@hotmail.com>
2021-03-08 16:53:54 -0300

    (08/03) Componente Select.

--- Origamid/modulo-4/aula7/appreact/src/App.js
+++ Origamid/modulo-4/aula7/appreact/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Input from './form/Input';
+import Select from './form/Select';
 
 const  App = () => {
   const [nome, setNome] = React.useState('')
   const [email, setEmail] = React.useState('')
+  const [produto, setProduto] = React.useState('')
 
   return (
     <form>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
-      <Input id="email" label="Email" value={email} setValue={setEmail} required/>
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <Select options={["Tablet", "Smartphone"]} value={produto} setValue={setProduto} />
+      <br />
       <button>
         Enviar
       </button>
