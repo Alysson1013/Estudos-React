ea803f013e02b1932c44c3f96fe2cd9b319ecc55
Alysson Eduardo <alysson6801@hotmail.com>
2021-03-08 19:01:24 -0300

    (08/03) Componente de Checkbox.

--- Origamid/modulo-4/aula7/appreact/src/App.js
+++ Origamid/modulo-4/aula7/appreact/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import Checkbox from './form/Checkbox';
 import Input from './form/Input';
+import Radio from './form/Radio';
 import Select from './form/Select';
 
 const  App = () => {
   const [nome, setNome] = React.useState('')
   const [email, setEmail] = React.useState('')
   const [produto, setProduto] = React.useState('')
+  const [cor, setCor] = React.useState('')
+  const [linguagens, setLinguagens] = React.useState([])
+  const [termos, setTermos] = React.useState([])
 
+  if(termos.length > 0){
+    console.log("enviado")
+  }
   return (
     <form>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
       <Select options={["Tablet", "Smartphone"]} value={produto} setValue={setProduto} />
       <br />
+      <Radio options={["Vermelho", "Azul"]} value={cor} setValue={setCor} />
+      <Checkbox options={["Javascript", "PHP", "Ruby"]} value={linguagens} setValue={setLinguagens} />
+      <Checkbox options={["Li e aceito os Termos."]} value={termos} setValue={setTermos} />
       <button>
         Enviar
       </button>
---
+++ Origamid/modulo-4/aula7/appreact/src/form/Radio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Radio = ({options, value, setValue, ...props}) => {
+    return (
+        <React.Fragment>
+            {options.map(option => (
+                <label key={option} type="radio">
+                    <input type="radio" value={option} checked={value === option} onChange={({target}) => setValue(target.value)} {...props} />
+                    {option}
+                </label>
+            ))}
+        </React.Fragment>
+    )
+}
+
+export default Radio
