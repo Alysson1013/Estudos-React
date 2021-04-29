bc63e6fbb23d73b7acc00b817abf475663e8d892
Alysson <alysson6801@hotmail.com>
2021-04-29 10:41:17 -0300

    login routes

--- Origamid/projeto-final/dogs/src/components/Login/Login.jsx
+++ Origamid/projeto-final/dogs/src/components/Login/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordReset from './LoginPasswordReset';
+import LoginPasswordLost from './LoginPasswordLost'
 
 const Login = () => {
     return (
         <div>
-            Login
+            <Routes>
+                <Route path="/" element={<LoginForm />} />
+                <Route path="criar" element={<LoginCreate />} />
+                <Route path="perdeu" element={<LoginPasswordReset />} />
+                <Route path="resetar" element={<LoginPasswordLost />} />
+            </Routes>
         </div>
     )
 }
