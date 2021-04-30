1d8d1c5a43231829c350224c7f695cb82dfe47b4
Alysson <alysson6801@hotmail.com>
2021-04-30 11:54:24 -0300

    context

--- Origamid/projeto-final/dogs/src/App.jsx
+++ Origamid/projeto-final/dogs/src/App.jsx
@@ -5,19 +5,22 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
 import Login from './components/Login/Login';
+import { UserStorage } from './UserContext'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path='/login' element={<Login />} />
+            <Route path='/login/*' element={<Login />} />
           </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
